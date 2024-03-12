71db8a14b8e60f59ba933ba3c4c1f2a4a98d1579
jeremiah-kellison <kellisonjj@gmail.com>
2024-03-12 14:52:07 -0700

    Used Prettier

--- script.user.js
+++ script.user.js
@@ -8,54 +8,43 @@
 // @description 3/6/2024, 12:58:45 PM
 // ==/UserScript==
 
-
-
-
 let setGridTemplateColumns = function () {
-    let ulElements = document.querySelector("#content_1903384 > div > div > ul");
-        ulElements.style.gridTemplateColumns = "repeat(5,18%)";
-   
-}
-
+  let ulElements = document.querySelector("#content_1903384 > div > div > ul");
+  ulElements.style.gridTemplateColumns = "repeat(5,18%)";
+};
 
 let searchBox = function () {
-    let searchBoxElement = document.querySelector("#content_1903384");
-    let searchBox = document.createElement("input");
-    searchBox.setAttribute("type", "text");
-    searchBoxElement.prepend(searchBox);
+  let searchBoxElement = document.querySelector("#content_1903384");
+  let searchBox = document.createElement("input");
+  searchBox.setAttribute("type", "text");
+  searchBoxElement.prepend(searchBox);
 
-
-    searchBox.addEventListener('input', function () {
-        let filter = searchBox.value.toUpperCase();
-        for (let i = 0; i < liArray.length; i++) {
-            let txtValue = liArray[i].innerText;
-            if (txtValue.toUpperCase().startsWith(filter )) {
-                liArray[i].style.display = "";
-               
-            } else {
-                liArray[i].style.display = "none";
-               
-            }
-        }
-    });
-}
+  searchBox.addEventListener("input", function () {
+    let filter = searchBox.value.toUpperCase();
+    for (let i = 0; i < liArray.length; i++) {
+      let txtValue = liArray[i].innerText;
+      if (txtValue.toUpperCase().startsWith(filter)) {
+        liArray[i].style.display = "";
+      } else {
+        liArray[i].style.display = "none";
+      }
+    }
+  });
+};
 
 let liArray = [];
 let ulArray = [];
 
 let gatherLiText = function () {
-    let listItems = document.querySelectorAll("#content_1903384 > div > div > ul > li");
-    for (let i = 0; i < listItems.length; i++) {
-        liArray.push(listItems[i]);
-        console.log(liArray);
-    }
-}
+  let listItems = document.querySelectorAll(
+    "#content_1903384 > div > div > ul > li"
+  );
+  for (let i = 0; i < listItems.length; i++) {
+    liArray.push(listItems[i]);
+    console.log(liArray);
+  }
+};
 
- 
-
-
-
-
-window.addEventListener("load", setGridTemplateColumns)
+window.addEventListener("load", setGridTemplateColumns);
 window.addEventListener("load", searchBox);
 window.addEventListener("load", gatherLiText);
